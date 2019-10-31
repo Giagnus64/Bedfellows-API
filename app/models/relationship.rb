0dcdab7a74e572a4b72be5804427abb401118317
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-10-30 21:14:20 -0400

    fixed an issue with relationship dependence on outings

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ApplicationRecord
     belongs_to :asker, class_name: 'User'
     belongs_to :askee, class_name: 'User'
-    has_many :outings
+    has_many :outings, dependent: :destroy
 
 end
